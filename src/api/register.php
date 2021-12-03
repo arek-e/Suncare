b97049d4667f16e1703385fdc4ec0787cdd1e9b0
Arekusanda01 <alexeklund460@gmail.com>
2021-12-03 10:18:17 +0100

    new database updated

--- src/api/register.php
+++ src/api/register.php
@@ -1,26 +1,25 @@
 <?php
-
 require_once 'db_connection.php';
-
 // Retrieve the contents of the php input request in the url
 $rest_json = file_get_contents("php://input");
-
 $_POST = json_decode($rest_json, true);
 
-
-// // Perform a query, check for error
-// if (!$conn -> query("INSERT INTO `user`(`email`, `password`, `firstName`, `lastName`, `phonenumber`,`cart_id`) VALUES ('". $_POST['email'] . "','" . $_POST['password'] . "','" . $_POST['firstName'] . "','" . $_POST['lastName'] . "','" . $_POST['phoneNum'] . "',4)")) {
+// Perform a query, check for error
+// if (!$conn -> query("INSERT INTO `users`(`email`, `password`, `first_name`, `last_name`, `phone_number`,`adminFlag`) VALUES ('". $_POST['email'] . "','" . $_POST['password'] . "','" . $_POST['first_name'] . "','" . $_POST['last_name'] . "','" . $_POST['phone_number'] . "', 0)")) {
 //     echo("Error description: " . $conn -> error);
 //   }
 
-$query ="INSERT INTO `user`(`email`, `password`, `firstName`, `lastName`, `phonenumber`,`cart_id`) 
-VALUES (
-    '". $_POST['email'] . "',
-    '" . $_POST['password'] . "',
-    '" . $_POST['firstName'] . "',
-    '" . $_POST['lastName'] . "','
-    " . $_POST['phoneNum'] . "',
-4)";
+if(!empty($_POST)){
+    $query ="INSERT INTO `users`(`email`, `password`, `first_name`, `last_name`, `phone_number`,`adminFlag`) 
+    VALUES (
+        '". $_POST['email'] . "',
+        '" . $_POST['password'] . "',
+        '" . $_POST['firstName'] . "',
+        '" . $_POST['lastName'] . "','
+        " . $_POST['phoneNum'] . "',
+        0
+    )";
+};
 
 // Send back a response for the frontend 
 $result = @mysqli_query($conn, $query);
